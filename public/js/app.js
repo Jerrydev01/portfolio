b817408a04539174403571a7b4c7c120bff005b5
zeus012345 <francisjerry360@gmail.com>
2022-04-20 12:08:40 +0100

    beta

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,20 @@
 
 const sections = document.querySelectorAll('section[id]');
 
-function scrollActive(){
-    const scrollY = window.pageYOffset;
-    console.log('scrollY');
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id');
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  console.log('scrollY');
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_scroll a[href*=' + sectionId + ']').classList.add('active-link');
-        }else{
-            document.querySelector('.nav_scroll a[href*=' + sectionId + ']').classList.remove('active-link');
-        }
-    });
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav_scroll a[href*=' + sectionId + ']').classList.add('active-link');
+    } else {
+      document.querySelector('.nav_scroll a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  });
 }
 window.addEventListener('scroll', scrollActive);
 
@@ -26,12 +26,12 @@ console.log(menu);
 const navMenueUl = document.querySelector(".navmenue-ul");
 console.log(navMenueUl);
 
-menu.addEventListener("click", () =>{
+menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   navMenueUl.classList.toggle("show");
 });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   menu.classList.remove("active");
   navMenueUl.classList.remove("show");
 }));
@@ -47,49 +47,48 @@ console.log(filterContainer);
 const projectItems = document.querySelectorAll('.project-item');
 console.log(projectItems);
 
-filterContainer.addEventListener("click", (event) =>{
-    if(event.target.classList.contains("filter-item")){
- 
-      // deactivate existing active 'filter-item'
-        filterContainer.querySelector(".active").classList.remove("active");
- 
-      // activate new 'filter-item'
-        event.target.classList.add("active");
- 
-        const filterValue = event.target.getAttribute("data-filter");
- 
-        projectItems.forEach((item) =>{
- 
-        if(item.classList.contains(filterValue) || filterValue === 'all'){
-            item.classList.remove("hide");
-             item.classList.add("show");
-        }
- 
-        else{
-            item.classList.remove("show");
-            item.classList.add("hide");
-        }
- 
-         });
-    }
-  });
+filterContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("filter-item")) {
 
-/*============== end of image gallery =========== */
-let fullImgBox = document.getElementById('fullImgBox');
-console.log(fullImgBox);
-let fullImg = document.getElementById('fullImg');
-console.log(fullImg);
+    // deactivate existing active 'filter-item'
+    filterContainer.querySelector(".active").classList.remove("active");
 
-function openFullImg(pic) {
-  fullImgBox.style.display = 'flex';
-  fullImg.src = pic;
+    // activate new 'filter-item'
+    event.target.classList.add("active");
+
+    const filterValue = event.target.getAttribute("data-filter");
+
+    projectItems.forEach((item) => {
+
+      if (item.classList.contains(filterValue) || filterValue === 'all') {
+        item.classList.remove("hide");
+        item.classList.add("show");
+      }
+
+      else {
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+
+    });
+  }
+});
+
+/*============== // Get each modal and close button=========== */
+
+const triggers = document.getElementsByClassName('trigger');
+const triggerArray = Array.from(triggers).entries();
+const modals = document.getElementsByClassName('modal');
+const closeButtons = document.getElementsByClassName('btn-close');
+
+for (let [index, trigger] of triggerArray) {
+  const toggleModal = () => {
+    modals[index].classList.toggle('show-modal');
+  }
+  trigger.addEventListener("click", toggleModal);
+  closeButtons[index].addEventListener("click", toggleModal);
+
 }
-
-function closeFullImg() {
-  fullImgBox.style.display = 'none';
-  
-}
-
 
 
 
@@ -110,8 +109,8 @@ var swiper = new Swiper(".mySwiperJerry", {
     el: ".swiper-pagination",
     clickable: true,
   },
-   // Responsive breakpoints
-   breakpoints: {
+  // Responsive breakpoints
+  breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
